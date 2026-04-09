5d699932dee8598a703f9177f3ac8a704228d096
Kristina-btl <kbogolyuk@yandex.ru>
2026-04-09 21:12:32 +0300

    Delete 03_lesson directory

--- 03_lesson/lesson3_task/lesson_3_task_1.py
+++
@@ -1,6 +0,0 @@
-from user3 import User
-
-my_user = User ("Mary", "Petrov")
-print(my_user.get_first_name())
-print(my_user.get_last_name())
-print(my_user.get_all_name())
--- 03_lesson/lesson3_task/smartphone.py
+++
@@ -1,6 +0,0 @@
-class Smartphone:
-    number = '+7 911 111 11 11'
-    def __init__(self, brand, model, number):
-        self.brand = brand
-        self.model = model
-        self.number = number
