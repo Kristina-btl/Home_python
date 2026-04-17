9c868887bdfd8c6ff7438c201a8bb143401476d6
Kristina-btl <kbogolyuk@yandex.ru>
2026-04-17 19:43:49 +0300

    lesson_06K

---
+++ 07_lesson/test_labirint.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+def test_cart_counter():
+    browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    browser.quit()
